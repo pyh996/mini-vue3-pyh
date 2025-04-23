b2e18a33ce51e66c603bc8248cd07996e6216a00
pyh996 <291410026@qq.com>
2025-04-23 22:56:45 +0800

    fix: 第三章-修改非computed的Effect.run 执行逻辑

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -94,9 +94,9 @@ export function triggerEffects(dep: Dep) {
     // }
   }
   for (const effect of effects) {
-    // if (!effect.computed) {
-    //   triggerEffect(effect)
-    // }
+    if (!effect.computed) {
+      triggerEffect(effect)
+    }
   }
 }
 /**
