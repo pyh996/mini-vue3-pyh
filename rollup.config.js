666c803d6123feb33aa60f6fcba635a24d812e9c
pyh996 <291410026@qq.com>
2025-04-23 16:22:29 +0800

    add: 第一章节搭建mini-vue3框架【手摸手带你实现一个vue3】

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default [{
+    input: 'packages/vue/src/index.ts',
+    output: [{
+        sourcemap: true,
+        file: 'packages/vue/dist/vue.js',
+        format: 'iife',
+        name: 'Vue'
+    }],
+    plugins: [
+        typescript({ sourceMap: true }),
+        resolve(),
+        commonjs()
+    ]
+}]
